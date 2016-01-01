f561a38298622d5a1d809782a6681f8e00e511e7
Fatih Kadir Akın <fakin@protel.com.tr>
2016-01-01 16:00:21 +0200

    Update SHA

--- guardian.rb
+++ guardian.rb
@@ -1,7 +1,7 @@
 class Guardian < Formula
   homepage "https://github.com/f/guardian"
   url "https://github.com/f/homebrew-guardian/archive/v0.0.1.zip"
-  sha256 ""
+  sha256 "a6d132838972893a29c6c2d88616192477c592d9a2b46027824e83bed1290b52"
 
   def install
   	bin.install "guardian"
