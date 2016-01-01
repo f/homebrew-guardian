5cca2444cf63a0553a32eb2597505bc6a77e3044
Fatih Kadir Akın <f@users.noreply.github.com>
2016-01-01 19:18:53 +0200

    Update guardian.rb

--- guardian.rb
+++ guardian.rb
@@ -1,9 +1,9 @@
 class Guardian < Formula
   desc "Guardian is a file watcher"
   homepage "http://github.com/f/guardian"
-  url "https://github.com/f/guardian/archive/v0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "5e67c0aac9336621edf9c97ab372613bed6f7ceedba4d16e603513f8a90a678c"
+  url "https://github.com/f/guardian/archive/v0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "4e98b8ca64aac757811c6ad519ddccb7e21b5ea69c3ef8dbeed04e94e3e1c7cb"
 
   def install
     bin.install "guardian"
