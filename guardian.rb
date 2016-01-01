a0b3d249ec43c606c0dd64c3e1532fcc37a458b5
Fatih Kadir Akın <fakin@protel.com.tr>
2016-01-01 15:56:59 +0200

    Initial commit

---
+++ guardian.rb
@@ -0,0 +1,9 @@
+class Guardian < Formula
+  homepage "https://github.com/f/guardian"
+  url "https://github.com/f/homebrew-guardian/archive/v0.0.1.zip"
+  sha256 ""
+
+  def install
+  	bin.install "guardian"
+  end
+end
