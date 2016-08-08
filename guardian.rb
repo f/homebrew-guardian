b9f9c95618a0d36178b4cb272208d7281858a6ff
Denny Trebbin <fibric@users.noreply.github.com>
2016-08-09 00:12:58 +0200

    libpcl - Added missing runtime dependeny

--- guardian.rb
+++ guardian.rb
@@ -4,6 +4,9 @@ class Guardian < Formula
   url "https://github.com/f/guardian/archive/v0.0.3.tar.gz"
   version "0.0.3"
   sha256 "ef58169ab43e38f17c97b4165369c34d6558e7940e4d6adecd1671e378c0760a"
+  revision 2
+  
+  depends_on "libpcl"
 
   def install
     bin.install "guardian"
