99ab4b3a84a23721ffb4100387bb27dee5ba6cd9
Fatih Kadir Akın <f@users.noreply.github.com>
2017-03-27 21:03:57 +0300

    Update guardian.rb

--- guardian.rb
+++ guardian.rb
@@ -1,10 +1,10 @@
 class Guardian < Formula
   desc "Guardian is a file watcher"
   homepage "http://github.com/f/guardian"
-  url "https://github.com/f/guardian/archive/v0.0.3.tar.gz"
-  version "0.0.3"
-  sha256 "ef58169ab43e38f17c97b4165369c34d6558e7940e4d6adecd1671e378c0760a"
-  revision 2
+  url "https://github.com/f/guardian/archive/v0.0.4.tar.gz"
+  version "0.0.4"
+  sha256 "ebc1cc4cb3dab84886e7ba78d40dc442c22ad1d065bddf3b5ba1ad38195518c7"
+  revision 3
   
   depends_on "libpcl"
 
